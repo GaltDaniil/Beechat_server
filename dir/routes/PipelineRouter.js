30cbaa8a27f2abb8f90e502d41f0ca831210be19
Daniil Galt <94684997+GaltDaniil@users.noreply.github.com>
2023-10-13 16:18:28 +0800

    Delete dir directory

--- dir/routes/PipelineRouter.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { getPipelines } from '../controllers/Pipeline.controller.js';
-const router = Router();
-router.get('/:id', getPipelines);
-export default router;
--- dir/routes/StageRouter.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { getManyStages } from '../controllers/Stage.controller.js';
-const router = Router();
-router.post('/many', getManyStages);
-export default router;
